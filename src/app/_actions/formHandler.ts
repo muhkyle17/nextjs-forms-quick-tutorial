aa9d02ad9481da15920c0120c6fff805e6a187c5
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-08-02 15:14:52 -0700

    Add logic for the form handler action

--- src/app/_actions/formHandler.ts
+++ src/app/_actions/formHandler.ts
@@ -1,3 +1,20 @@
 'use server'
 
-export const formHandlerAction = async (formData: FormData) => {}
+import { dealSchema } from '../_schemas/deal'
+
+export const formHandlerAction = async (formData: FormData) => {
+  const unvalidatedDeal = {
+    name: formData.get('name'),
+    link: formData.get('link'),
+    couponCode: formData.get('couponCode'),
+    discount: formData.get('discount'),
+  }
+
+  const validated = dealSchema.safeParse(unvalidatedDeal)
+
+  if (!validated.success) {
+    return {}
+  } else {
+    return { successMsg: 'Deal added successfully' }
+  }
+}
