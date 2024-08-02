76441bfe7dce2954955ad9c4a145b2bc329b195e
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-08-02 11:42:02 -0700

    Add form elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import DealForm from '../components/DealForm'
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+    <main className='flex min-h-screen max-w-4xl mx-auto flex-col items-center justify-between p-24'>
       <DealForm />
     </main>
   )
--- src/components/DealForm.tsx
+++ src/components/DealForm.tsx
@@ -1,8 +1,57 @@
 const DealForm = () => {
   return (
-    <div>
-      <p>Deal Form</p>
-    </div>
+    <form className='w-full'>
+      <div className='flex flex-col gap-y-4'>
+        <div>
+          <label className='block' htmlFor='name'>
+            Name
+          </label>
+          <input
+            type='text'
+            name='name'
+            id='name'
+            className='w-full p-2 rounded-md text-gray-900'
+          />
+        </div>
+
+        <div>
+          <label className='block' htmlFor='link'>
+            Link (must start with https://)
+          </label>
+          <input
+            type='text'
+            name='link'
+            id='link'
+            className='w-full p-2 rounded-md text-gray-900'
+          />
+        </div>
+
+        <div>
+          <label className='block' htmlFor='couponCode'>
+            Coupon Code
+          </label>
+          <input
+            type='text'
+            name='couponCode'
+            id='couponCode'
+            className='w-full p-2 rounded-md text-gray-900'
+          />
+        </div>
+
+        <div>
+          <label className='block' htmlFor='discount'>
+            Discount (%)
+          </label>
+          <input
+            type='text'
+            name='discount'
+            id='discount'
+            className='w-full p-2 rounded-md text-gray-900'
+          />
+        </div>
+        <button>Submit</button>
+      </div>
+    </form>
   )
 }
 
